f59c95376fad875a1b0450a2c85bb0339a896784
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-04-15 23:02:43 +0600

    Create monotonic queue.cpp

---
+++ monotonic queue.cpp
@@ -0,0 +1,15 @@
+struct monotonic_queue
+{
+    deque<pair<int,int> >dq;
+    void add(int val,int in)
+    {
+        while(!dq.empty()&&dq.back().first>=val) //strictly increasing order
+            dq.pop_back();
+        dq.push_back({val,in});
+    }
+    void del(int in)
+    {
+        if(!dq.empty() && dq.front().second==in)
+            dq.pop_front();
+    }
+};
