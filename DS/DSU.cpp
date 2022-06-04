c7b9dab2eece4031ffa75c7f30fb6fb8f41b49c6
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-06-04 15:22:23 +0600

    Update DSU.cpp

--- DS/DSU.cpp
+++ DS/DSU.cpp
@@ -1,14 +1,34 @@
-int par[N+5];
-void init()
+struct DSU
 {
-   fr(i,1,N) par[i]=i;
-}
-int get(int n)
-{
-   if(par[n]==n) return n;
-   return par[n]=get(par[n]);
-}
-void uni(int a,int b)
-{
-   par[a]=b;
-}
+    vector<int>par,sz;
+    DSU(int n)
+    {
+        par.assign(n+1,0);
+        sz.assign(n+1,1);
+        for(int i=1; i<=n; i++)
+            par[i]=i;
+    }
+    int get_parent(int n)
+    {
+        if(par[n]==n)
+            return n;
+        return par[n]=get_parent(par[n]);
+    }
+    void union_set(int x,int y)
+    {
+        x=get_parent(x);
+        y=get_parent(y);
+        if(x!=y)
+        {
+            if(sz[x]<sz[y])
+                swap(x,y);
+            par[y]=x;
+            sz[x]+=sz[y];
+            sz[y]=0;
+        }
+    }
+    int get_size(int n)
+    {
+        return sz[get_parent(n)];
+    }
+};
