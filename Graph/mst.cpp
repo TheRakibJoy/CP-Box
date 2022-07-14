1e8455032f187c7f398cc1a8d3abf61d84bce3ef
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-07-15 00:29:28 +0600

    Update mst.cpp

--- Graph/mst.cpp
+++ Graph/mst.cpp
@@ -1,3 +1,37 @@
+struct DSU
+{
+    vector<int>par,sz;
+    DSU(int n)
+    {
+        par.assign(n+1,0);
+        sz.assign(n+1,1);
+        for(int i=1; i<=n; i++)
+            par[i]=i;
+    }
+    int get_parent(int n)
+    {
+        if(par[n]==n)
+            return n;
+        return par[n]=get_parent(par[n]);
+    }
+    void union_set(int x,int y)
+    {
+        x=get_parent(x);
+        y=get_parent(y);
+        if(x!=y)
+        {
+            if(sz[x]<sz[y])
+                swap(x,y);
+            par[y]=x;
+            sz[x]+=sz[y];
+            sz[y]=0;
+        }
+    }
+    int get_size(int n)
+    {
+        return sz[get_parent(n)];
+    }
+};
 struct edge
 {
     int u, v, w;
@@ -7,33 +41,17 @@ struct edge
     }
 };
 vector<edge> e;
-ll par[N+5];
-void init(ll n)
-{
-    fr(i,1,n) par[i]=i;
-}
-ll get(ll n)
-{
-    if(par[n]==n)
-        return n;
-    return par[n]=get(par[n]);
-}
-void uni(ll a,ll b)
-{
-    par[a]=b;
-}
 ll mst(int n)
 {
     sort(e.begin(), e.end());
-    init(n);
+    DSU d(n);
     ll cnt = 0, s = 0;
     for (int i = 0; i < (int)e.size(); i++)
     {
-        int u = get(e[i].u);
-        int v = get(e[i].v);
-        if (u != v)
+        int u=e[i].u,v=e[i].v;
+        if (d.get_parent(u) != d.get_parent(v))
         {
-            uni(u,v);
+            d.union_set(u,v);
             cnt++;
             s += (ll)e[i].w;
             if (cnt == n - 1)
