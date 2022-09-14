b4ae99a84e5cf7133f8814269478e7c42db4bccc
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-09-14 11:39:20 +0600

    Create new.cpp

---
+++ Template/new.cpp
@@ -0,0 +1,59 @@
+/*starting with the name of almighty ALLAH*/
+/*solved by RAKIB JOY*/
+// Practice is the only shortcut to improve
+#include<bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define dbg(x) cout<<x<<endl;
+#define mp(x,y) make_pair(x,y)
+#define yes dbg("YES")
+#define no dbg("NO")
+#define tst int t;cin>>t;while(t--)
+#define srt(v) sort(v.begin(),v.end());
+#define rsrt(v) sort(v.rbegin(),v.rend());
+#define rj ios::sync_with_stdio(false);cin.tie(0);
+#define rvs(v) reverse(v.begin(),v.end());
+#define F first
+#define S second
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define PI 2*acos(0.0)
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define ld long double
+#define nl '\n'
+const int N=1e5;
+using namespace std;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_rand(int l, int r)
+{
+    return uniform_int_distribution<int>(l, r) (rng);
+}
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+void solve()
+{
+
+}
+int main()
+{
+    rj
+    //ll t;scanf("%lld",&t);fr(i,1,t) printf("Case %lld: ",i),solve();
+    solve();
+    return 0;
+}
