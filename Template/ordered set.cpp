76718a8a84eb5ae966a1d64c588ef5b197e76038
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2022-09-24 17:36:54 +0600

    Update ordered set.cpp

--- Template/ordered set.cpp
+++ Template/ordered set.cpp
@@ -5,3 +5,6 @@ template<typename T> using ordered_set =tree<T, null_type,less<int>, rb_tree_tag
 //order_of_key (k) : Number of items strictly smaller than k .
 //find_by_order(k) : K-th element in a set (counting from zero).
 template<typename T> using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+Erase -
+if(os.upper_bound(x)!=os.end())
+    os.erase(os.upper_bound(x));
