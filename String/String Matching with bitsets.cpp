6a43d16d57056038d47ade7b776889d88626b759
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2023-01-18 18:36:11 +0600

    Update String Matching with bitsets.cpp

--- String/String Matching with bitsets.cpp
+++ String/String Matching with bitsets.cpp
@@ -1,35 +1,42 @@
-//From YouKnowWho
-#include<bits/stdc++.h>
-using namespace std;
-
-const int N = 1e5 + 9;
-vector<int> v;
-bitset<N>bs[26], oc;
-int main() {
-  int i, j, k, n, q, l, r;
-  string s, p;
-  cin >> s;
-  for(i = 0; s[i]; i++) bs[s[i] - 'a'][i] = 1;
-  cin >> q;
-  while(q--) {
-    cin >> p;
-    oc.set();
-    for(i = 0; p[i]; i++) oc &= (bs[p[i] - 'a'] >> i);
-    cout << oc.count() << endl; // number of occurences
-    int ans = N, sz = p.size();
-    int pos = oc._Find_first();
-    v.push_back(pos);
-    pos = oc._Find_next(pos);
-    while(pos < N) {
-      v.push_back(pos);
-      pos = oc._Find_next(pos);
+//Here pos stores every starting pos of ss in s as a substring
+bitset<N>bs[26],p;
+void solve()
+{
+    string s;
+    cin>>s;
+    int a=s.size();
+    for(int i=0;i<a;i++)
+    {
+        bs[s[i]-'a'][i]=1;
     }
-    for(auto x : v) cout << x << ' '; // position of occurences
-    cout << endl;
-    v.clear();
-    cin >> l >> r; // number of occurences from l to r,where l and r is 1-indexed
-    if(sz > r - l + 1) cout << 0 << endl;
-    else cout << (oc >> (l - 1)).count() - (oc >> (r - sz + 1)).count() << endl;
-  }
-  return 0;
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int x;
+        string ss;
+        cin>>x>>ss;
+        int sz=ss.size();
+        p.set();
+        for(int i=0;i<sz;i++)
+        {
+            p&=bs[ss[i]-'a']>>i;
+        }
+        vector<int>pos;
+        for(int i=p._Find_first();i<a;i=p._Find_next(i))
+        {
+            pos.emplace_back(i);
+        }
+        if(pos.size()<x)
+        {
+            cout<<"-1\n";
+            continue;
+        }
+        int ans=INT_MAX;
+        for(int i=x-1;i<pos.size();i++)
+        {
+            ans=min(ans,pos[i]-pos[i-x+1]+sz);
+        }
+        cout<<ans<<'\n';
+    }
 }
