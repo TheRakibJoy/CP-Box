b356ddf91263b6102810cb2c48e72262a4bca24e
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2024-03-02 16:19:42 +0600

    Update LCA.cpp

--- Graph/LCA.cpp
+++ Graph/LCA.cpp
@@ -1,55 +1,52 @@
-const int sz=200005;
-int sptab[sz][22],lvl[sz],par[sz];
-void Init_LCA(int n)
+const int B = 20;
+int par[N][B], lvl[N];
+vector<int>adj[N];
+void dfs(int n, int p = 0)
 {
-    for(int i=0; i<=n; i++)
-    {
-        for(int j=0; j<22; j++)
-            sptab[i][j]=-1;
-    }
-    for(int i=1 ; i<=n ; i++)
-        sptab[i][0] = par[i];
-    for(int j=1 ; (1<<j)<=n ; j++)
-    {
-        for(int i=1 ; i<=n ; i++)
-        {
-            if(sptab[i][j-1] != -1)
-            {
-                sptab[i][j] = sptab[sptab[i][j-1]][j-1];
-            }
-        }
-    }
+	par[n][0] = p;
+	lvl[n] = lvl[p] + 1;
+	for (int i = 1; i < B; i++)
+	{
+		par[n][i] = par[par[n][i - 1]][i - 1];
+	}
+	for (auto it : adj[n])
+	{
+		if (it != p)
+		{
+			dfs(it, n);
+		}
+	}
 }
-int get_LCA(int p,int q)
+int get_LCA(int p, int q)
 {
-    if(lvl[p] < lvl[q])
-        swap(p,q);
-    int Log=log2(lvl[p])+1;
-    for(int i=Log ; i>=0 ; i--)
-    {
-        if(lvl[p]-(1<<i) >= lvl[q])
-            p = sptab[p][i];
-    }
-    if(p == q)
-        return p;
-    for(int i=Log ; i>=0 ; i--)
-    {
-        if(sptab[p][i]!=-1 && sptab[p][i]!=sptab[q][i])
-        {
-            p=sptab[p][i];
-            q=sptab[q][i];
-        }
-    }
-    return par[p];
+	if (lvl[p] < lvl[q])
+		swap(p, q);
+	int Log = log2(lvl[p]) + 1;
+	for (int i = Log ; i >= 0 ; i--)
+	{
+		if (lvl[p] - (1 << i) >= lvl[q])
+			p = par[p][i];
+	}
+	if (p == q)
+		return p;
+	for (int i = Log ; i >= 0 ; i--)
+	{
+		if (par[p][i] != par[q][i])
+		{
+			p = par[p][i];
+			q = par[q][i];
+		}
+	}
+	return par[p][0];
 }
-int get_kth(ll n,ll k)
+int get_kth(int n, int k)
 {
-    ll Log=log2(k);
-    ll p=n;
-    for(int i=0; i<=Log; i++)
-    {
-        if(k&(1<<i))
-            p=sptab[p][i];
-    }
-    return p;
+	int Log = log2(k);
+	int p = n;
+	for (int i = 0; i <= Log; i++)
+	{
+		if (k & (1 << i))
+			p = par[p][i];
+	}
+	return p;
 }
