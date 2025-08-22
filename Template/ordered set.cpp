41f64ba1e7123a76715a64dee200f57e64f4c293
Md. Rakib Hossain <70007412+TheRakibJoy@users.noreply.github.com>
2025-08-22 22:22:37 +0600

    Update ordered set.cpp

--- Template/ordered set.cpp
+++ Template/ordered set.cpp
@@ -1,7 +1,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template<typename T> using ordered_set =tree<T, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T> using ordered_set =tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 //order_of_key (k) : Number of items strictly smaller than k .
 //find_by_order(k) : K-th element in a set (counting from zero).
 template<typename T> using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
